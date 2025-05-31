8f32dc2494d1880860fe17c902c0bc1acd272edc
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-05-31 15:03:42 +0200

    feat: link task service layer to data access object

--- src/main/java/com/lab/task_management_api/service/TaskService.java
+++ src/main/java/com/lab/task_management_api/service/TaskService.java
@@ -3,6 +3,9 @@ package com.lab.task_management_api.service;
 import com.lab.task_management_api.dao.TaskDao;
 import com.lab.task_management_api.model.Task;
 
+import java.util.List;
+import java.util.Optional;
+
 public class TaskService {
     TaskDao taskDao;
 
@@ -13,4 +16,26 @@ public class TaskService {
     public void createTask(Task task){
         this.taskDao.create(task);
     }
+
+    public Optional<Task> retrieveTask(int taskId){
+        return taskDao.findOne(taskId);
+    }
+
+    public List<Task> retrieveAllTasks(){
+        return taskDao.findAll();
+    }
+
+    public Task updateTask(Task task){
+        return taskDao.update(task);
+    }
+
+    public Task updateTaskProgress(int taskId, int status){
+        return taskDao.updateProgress(taskId, status);
+    }
+
+    public List<Task> filterTaskStatus(int status){
+        return taskDao.filterStatus(status);
+    }
+
+
 }
